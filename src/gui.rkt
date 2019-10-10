5aafcebf10b3b7a23f18178a546b1fe6d7ca14e3
machetazo <fabianmontero1998@gmail.com>
2019-10-09 20:26:43 -0600

    gui: versión básica y funcional de un gui
    	modified:   src/gui.rkt

--- src/gui.rkt
+++ src/gui.rkt
@@ -1,1 +1,23 @@
 ; gui
+(require racket/gui/base)
+
+; crea la frame y le pone título
+(define frame (new frame% [label "BlaCEk-Jack"]))
+
+; crea un mensajito cute
+(define msg (new message% [parent frame]
+  	    [label "Todo está bien."]))
+
+; crea un boton
+(new button% [parent frame]
+     	     [label "Haga click aqui"]
+	     ; este es el callback para ejecutarse cuando se apreta el botón
+	     [callback (lambda (button event)
+			 (send msg set-label "Alguien hizo click en el botón"))])
+
+; muestra la ventana
+(send frame show #t)
+
+
+
+
