55a7a64cdfba8d8d5af9e43bd45724d4381a8156
aleklnx <diaza913@gmail.com>
2019-10-15 17:10:47 -0600

    Añade diseño de funciones para GUI, añade diagrama de flujo del proceso general del programa 	new file:   docs/design/diagrama de funciones.pdf 	modified:   docs/design/funciones 	modified:   src/engine.rkt

--- src/engine.rkt
+++ src/engine.rkt
@@ -1,1 +1,22 @@
-; engine
+;
+; Agrega una carta a las cartas de un jugador determinado.
+;
+; Parámetros:
+;   name: Nombre del jugador buscado
+;   players: lista de jugadores
+;   deck: lista de cartas disponibles
+; Retorna:
+;   La lista players pero el jugador con nombre igual a name adqduiere una carta más del deck. 
+;
+
+(define (deal name players deck)
+  (cond ((empty? players)
+         (list))
+        ((equal? name (caar players))
+         (cons (cons name
+                     (list (append (cadar players)
+                                   (list (car deck)))))
+               (deal name (cdr players) deck)))
+        (else
+         (cons (car players)
+                 (deal name (cdr players) deck)))))
